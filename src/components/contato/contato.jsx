e7ab9bce8f87f06941bddd02343dfffdbd1a2140
Felipe-Zucoli <zucolifelipe@gmail.com>
2024-10-14 20:50:46 -0300

    Att contato, troca de texto

--- src/components/contato/contato.jsx
+++ src/components/contato/contato.jsx
@@ -6,7 +6,7 @@ const Contato = () => {
     return (
         <div className="containerContato">
         <p>Obrigado por ver meu portfolio!<br></br><br></br>
-        Caso queira entrar em contato, segue meus links de contato.</p>
+        Caso queira entrar em contato, segue meus links.</p>
         <ul className="linkContato">
             <li><a href="https://www.linkedin.com/in/felipe-zucoli-51b571202/" target="_blank" className="linkeIconContato"><FaLinkedin></FaLinkedin></a></li>
             <li><a href="https://wa.me/5544991536070" target="_blank" className="whatsIconContato"><FaWhatsapp></FaWhatsapp></a></li>
